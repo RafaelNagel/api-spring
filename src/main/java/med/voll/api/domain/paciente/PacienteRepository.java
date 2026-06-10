956cd83a68bdd6d07fb7cf7d8347705f110a609b
RafaelNagel <rafaelnageldesouza@gmail.com>
2026-06-10 20:32:33 -0300

    feat: adiciona validadores, table para consultas e controller para a consulta

--- src/main/java/med/voll/api/domain/paciente/PacienteRepository.java
+++ src/main/java/med/voll/api/domain/paciente/PacienteRepository.java
@@ -1,9 +1,12 @@
 package med.voll.api.domain.paciente;
 
+import jakarta.validation.constraints.NotNull;
 import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.domain.Pageable;
 
 public interface PacienteRepository extends JpaRepository<Paciente, Long> {
     Page<Paciente> findAllByAtivoTrue(Pageable paginacao);
+
+    boolean findAtivoById(@NotNull Long aLong);
 }
